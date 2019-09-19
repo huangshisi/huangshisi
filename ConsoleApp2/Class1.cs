5e130210ae2f556b174291dcba5cf439adfa9b60
1625941327 <1625941327@qq.com>
2019-09-19 16:57:18 +0800

    1

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1234");
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Numerics;
+﻿//using System;
+//using System.Numerics;
 
-namespace ConsoleApp2
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string s1 = "a string";
-            string s2 = s1;
-            Console.WriteLine("s1 is"+s1);
-            Console.WriteLine("s2 is" + s2);
-        }
-    }
-}
+//namespace ConsoleApp2
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string s1 = "a string";
+//            string s2 = s1;
+//            Console.WriteLine("s1 is"+s1);
+//            Console.WriteLine("s2 is" + s2);
+//        }
+//    }
+////}
