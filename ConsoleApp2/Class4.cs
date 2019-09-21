dabb0d873184cac735782063c4f79d7b67a93dae
1625941327 <1625941327@qq.com>
2019-09-21 21:42:53 +0800

    5

--- ConsoleApp2/Class4.cs
+++ ConsoleApp2/Class4.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace ConsoleApp2
-{
-    class Class4
-    {
-        public class MathLib
-        {
-           public int Add(int x,int y)
-            {
-                return x + y;
+//namespace ConsoleApp2
+//{
+//    class Class4
+//    {
+//        public class MathLib
+//        {
+//           public int Add(int x,int y)
+//            {
+//                return x + y;
                
-            }
+//            }
 
-        }
-        public static void Main()
-        {
-            MathLib mathlib = new MathLib();
-            Console.WriteLine(mathlib.Add(7, 8));
+//        }
+//        public static void Main()
+//        {
+//            MathLib mathlib = new MathLib();
+//            Console.WriteLine(mathlib.Add(7, 8));
 
-        }
-    }
-}
+//        }
+//    }
+//}
---
+++ ConsoleApp2/Class5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Class5
+    {
+
+       class MathTest
+        {
+            public int value; 
+            public int Getsquare()
+            {
+                return value * value;
+            }
+            public static int Getquareof(int x)
+            {
+                return x * x;
+
+            }
+            public static double GetPi()
+            {
+                return 3.14;
+            }
+        }
+        static void Main()
+        {
+            Console.WriteLine("pi is" + MathTest.GetPi());
+            int x = MathTest.Getquareof(5);
+            Console.WriteLine("quareof 5 is" + x);
+            MathTest math = new MathTest();
+            math.value = 30;
+            Console.WriteLine(math.Getsquare());
+        }
+
+    }
+}
