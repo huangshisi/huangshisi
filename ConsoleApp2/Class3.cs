b4dbe957c4a419aa0514f67cd2ba19879bd0ab40
1625941327 <1625941327@qq.com>
2019-09-21 17:45:51 +0800

    3

---
+++ ConsoleApp2/Class3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Class3
+    {
+         public  static int Main(string[] args)
+        { 
+            for(int i=0;i< args.Length;i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+            return 0;
+        }
+    }
+}
