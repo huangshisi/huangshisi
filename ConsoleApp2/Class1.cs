27d3f714027475389c41fc25f2145da30a52c536
1625941327 <1625941327@qq.com>
2019-09-21 16:46:42 +0800

    1

--- ConsoleApp2/Class1.cs
+++ ConsoleApp2/Class1.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace ConsoleApp2
-{
-    class Class1
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("1234");
-        }
-    }
-}
+//namespace ConsoleApp2
+//{
+//    class Class1
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("1234");
+//        }
+//    }
+//}
---
+++ ConsoleApp2/Class2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Class2
+    {
+        public enum TimeOfDay
+        {
+            Morning=0,
+            Afternoom=1,
+            Evening=2,
+        }
+
+
+        public static int Main()
+        {
+            WriteGeerting(TimeOfDay.Morning);
+                return 0;
+        }
+        static void WriteGeerting(TimeOfDay timeOfDay)
+
+        {
+            switch (timeOfDay)
+            {
+                case TimeOfDay.Morning:
+                    Console.WriteLine("good moring");
+                    break;
+                case TimeOfDay.Afternoom:
+                    Console.WriteLine("good Afternoom");
+                    break;
+                case TimeOfDay.Evening:
+                    Console.WriteLine("good evening");
+                    break;
+                default:
+                    Console.WriteLine("hello word");
+                    break;
+            }
+        }
+    }
+}
