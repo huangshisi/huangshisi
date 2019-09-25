5053dde1a13092539f4db8c44f08acc37801be64
1625941327 <1625941327@qq.com>
2019-09-25 22:13:47 +0800

    4

--- ConsoleApp2/Class1.cs
+++ ConsoleApp2/Class1.cs
@@ -1,14 +1,38 @@
-﻿using System;
+﻿//using System;
 //using System.Collections.Generic;
 //using System.Text;
-
 //namespace ConsoleApp2
 //{
 //    class Class1
 //    {
+
+//        class Class
+//        {
+//            public int value;
+//            public int GetSquare()
+//            {
+//                return value * value;
+//            }
+//            public static int GetSquareOf(int x)
+//            {
+//                return x * x;
+//            }
+
+//            public static double Getpi()
+//            {
+//                return 3.14;
+//            }
+//        }
+
 //        static void Main(string[] args)
 //        {
-//            Console.WriteLine("1234");
+//            Console.WriteLine("p is" + Class.Getpi());
+//            int x = Class.GetSquareOf(5);
+//            Console.WriteLine("i is" + Class.GetSquareOf(5));
+//            Class c = new Class();
+//            c.value = 10;
+//            Console.WriteLine("pi is" + c.GetSquare());
+
 //        }
 //    }
 //}
--- ConsoleApp2/Class3.cs
+++ ConsoleApp2/Class3.cs
@@ -1,18 +1,72 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-//namespace ConsoleApp2
-//{
-//    class Class3
-//    {
-//         public  static int Main(string[] args)
-//        { 
-//            for(int i=0;i< args.Length;i++)
-//            {
-//                Console.WriteLine(args[i]);
-//            }
-//            return 0;
-//        }
-//    }
-//}
+namespace ConsoleApp2
+{
+    //class MainEntryPoint
+    //{
+    //    class MathTest
+    //    {
+    //        public int value;
+    //        public int GetSquare()
+    //        {
+    //            return value * value;
+    //        }
+    //        public static int GetSquareOf(int x)
+    //        {
+    //            return x * x;
+    //        }
+    //        public static double GetPi()
+
+    //        {
+    //            return 3.14;
+    //        }
+    //    }
+    //    static void Main()
+    //    {
+    //        WeakReference weakReference = new WeakReference(new MathTest ());
+    //        MathTest math;
+    //        if (weakReference .IsAlive )
+    //        {
+    //            math = weakReference.Target as MathTest;
+    //            math.value = 30;
+    //            Console.WriteLine("" + MathTest.GetPi());
+    //            Console.WriteLine("square is" + math.GetSquare());
+
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("1");
+    //        }
+    //        GC.Collect();
+    //            if (weakReference .IsAlive )
+    //        {
+    //            math = weakReference.Target as MathTest;
+    //        }else
+    //        {
+    //            Console.WriteLine("1");
+    //        }
+    //    }
+    //}
+    class Protest
+    {
+        static void SomeFunction(int[] ints ,int i )
+        {
+            ints[0]= 100;
+            i = 100;
+        }
+        static void Main()
+        {
+            int i = 0;
+            int[] ints = { 0, 1, 2, 4, 8 };
+            Console.WriteLine("i" + i);
+            Console.WriteLine("ints[0]"+ints[0]);
+            Console.WriteLine("Calling someFunction");
+            SomeFunction(ints,i);
+            Console.WriteLine("i"+i);
+            Console.WriteLine("int[0]"+ints[0]);
+
+        }
+    }
+}
