e2ca968fc8d710f4389da0944e03f019ac4ba38a
1625941327 <1625941327@qq.com>
2019-09-25 11:06:36 +0800

    5

--- ConsoleApp2/Class5.cs
+++ ConsoleApp2/Class5.cs
@@ -1,38 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace ConsoleApp2
-{
-    class Class5
-    {
+//namespace ConsoleApp2
+//{
+//    class Class5
+//    {
 
-       class MathTest
-        {
-            public int value; 
-            public int Getsquare()
-            {
-                return value * value;
-            }
-            public static int Getquareof(int x)
-            {
-                return x * x;
+//       class MathTest
+//        {
+//            public int value;
+//            public int Getsquare()
+//            {
+//                return value * value;
+//            }
+//            public static int Getquareof(int x)
+//            {
+//                return x * x;
 
-            }
-            public static double GetPi()
-            {
-                return 3.14;
-            }
-        }
-        static void Main()
-        {
-            Console.WriteLine("pi is" + MathTest.GetPi());
-            int x = MathTest.Getquareof(5);
-            Console.WriteLine("quareof 5 is" + x);
-            MathTest math = new MathTest();
-            math.value = 30;
-            Console.WriteLine(math.Getsquare());
-        }
+//            }
+//            public static double GetPi()
+//            {
+//                return 3.14;
+//            }
+//        }
+//        static void Main()
+//        {
+//            Console.WriteLine("pi is" + MathTest.GetPi());
+//            int x = MathTest.Getquareof(5);
+//            Console.WriteLine("quareof 5 is" + x);
+//            MathTest math = new MathTest();
+//            math.value = 30;
+//            Console.WriteLine(math.Getsquare());
+//        }
 
-    }
-}
+//    }
+//}
