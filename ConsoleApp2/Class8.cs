bbafb7235485f06b8e73a225f30b3eb7ddd240f4
1625941327 <1625941327@qq.com>
2019-09-26 23:05:15 +0800

    boxing

--- ConsoleApp2/Class8.cs
+++ ConsoleApp2/Class8.cs
@@ -1,65 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace ConsoleApp2
-{
-    class gouzuohanshu
-    { 
-    public abstract class GenericCustomer
-    {
-        private string name;
-        public GenericCustomer()
-        {
-            name = "<no name>";
-        }
-        public GenericCustomer(string name)
-        {
-            this.name = name;
-        }
-        public string Name
-        {
-            get
-            {
-                return name;
-            }
-            set
-            {
-                name = value;
-            }
+//namespace ConsoleApp2
+//{
+//    class gouzuohanshu
+//    { 
+//    public abstract class GenericCustomer
+//    {
+//        private string name;
+//        public GenericCustomer()
+//        {
+//            name = "<no name>";
+//        }
+//        public GenericCustomer(string name)
+//        {
+//            this.name = name;
+//        }
+//        public string Name
+//        {
+//            get
+//            {
+//                return name;
+//            }
+//            set
+//            {
+//                name = value;
+//            }
 
-        }
+//        }
 
           
-        }
-    class Nevermore60Customer : GenericCustomer
-    {
-        private uint highCostMinutesUsed;
-        private string referremer;
+//        }
+//    class Nevermore60Customer : GenericCustomer
+//    {
+//        private uint highCostMinutesUsed;
+//        private string referremer;
 
-        public Nevermore60Customer()
-        {
-        }
+//        public Nevermore60Customer()
+//        {
+//        }
 
-        public Nevermore60Customer(string name) : base(name)
-        {
+//        public Nevermore60Customer(string name) : base(name)
+//        {
 
-        }
-        public Nevermore60Customer(string name, string referremer) : base(name)
-        {
-            this.referremer = referremer;
-        }
+//        }
+//        public Nevermore60Customer(string name, string referremer) : base(name)
+//        {
+//            this.referremer = referremer;
+//        }
 
 
-    }
-    class C
-    {
-        public static void Main()
-        {
-            GenericCustomer customer = new Nevermore60Customer();
+//    }
+//    class C
+//    {
+//        public static void Main()
+//        {
+//            GenericCustomer customer = new Nevermore60Customer();
                 
 
-        }
-    }
-    }
-}
+//        }
+//    }
+//    }
+//}
---
+++ ConsoleApp2/boxing.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace  Boxing
+{
+    class Boxing
+    {
+        public static void Main()
+        {
+            var list = new ArrayList();
+            list.Add(44);
+            int i1 = (int)list[0];
+            foreach (int i2 in list)
+            {
+                Console.WriteLine(i2);
+
+            }
+            var list1 = new List<int>();
+            list1.Add(44);
+            int i3 = list1[0];
+            foreach (int i4 in list1  )
+            {
+                Console.WriteLine(i4);
+            }
+        }
+    }
+}
+
