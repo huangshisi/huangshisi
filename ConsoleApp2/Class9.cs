98aaeaed59fcdea41d7e9722d8a138d313409b34
1625941327 <1625941327@qq.com>
2019-09-28 22:30:06 +0800

    Functions of pan-classes

---
+++ ConsoleApp2/Class9.cs
@@ -0,0 +1,86 @@
+﻿using NPOI.SS.Formula.Functions;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace Functions.of
+{ 
+    class class9
+    {
+        public class DocumentManager<T>
+        {
+            private readonly Queue<T> documentQueue = new Queue<T>();
+            public void AddDocument(T doc)
+            {
+                lock (this)
+                {
+                    documentQueue.Enqueue(doc);
+                }
+
+            }
+            public bool IsDocumentAvailable
+            {
+                get { return documentQueue.Count > 0; }
+            }
+
+            public T GetDocument()
+            {
+                T doc = default(T);
+                lock (this)
+                {
+                    doc = documentQueue.Dequeue();
+                }
+                return doc;
+            }
+            public interface TDocument
+            {
+                string Title { get; set; }
+                string Content { get; set; }
+            }
+            public class Document : TDocument
+            {
+                public Document()
+                {
+
+                }
+                public Document(string title, string content)
+                {
+                    this.Title = title;
+                    this.Content = content;
+                }
+                public string Title { get; set; }
+                public string Content { get; set; }
+            }
+
+            public void DisplayAllDocuments()
+            {
+                foreach (T doc in documentQueue)
+                {
+                    Console.WriteLine(((TDocument)doc).Title);
+                }
+            }
+
+            private static void Main()
+            {
+                var dm = new DocumentManager<Document>();
+                dm.AddDocument(new Document("Title A", "Sample A"));
+                dm.AddDocument(new Document("Title B", "sample B"));
+                dm.DisplayAllDocuments();
+                if (dm.IsDocumentAvailable)
+                {
+                    Document d = dm.GetDocument();
+                    Console.WriteLine(d.Content);
+                }
+            }
+        }
+        
+
+
+
+
+
+    }
+
+    }
+
--- ConsoleApp2/Create generic classes.cs
+++ ConsoleApp2/Create generic classes.cs
@@ -1,74 +1,74 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace ConsoleApp2
-{
-    class Creategenericclasses
-    {
-        public class LinkedListNode<T>
-        {
-            public LinkedListNode(T value)
-            {
-                this.Value = value;
-            }
-            public T Value { get; internal set; }
-            public LinkedListNode<T> Next { get; internal set; }
-            public LinkedListNode<T> Prev { get; internal set; }
-        }
-        public class LinkedList<T> : IEnumerable<T>
-        {
-            public LinkedListNode<T> First { get; private set; }
-            public LinkedListNode<T> Last { get; private set; }
-            public LinkedListNode<T> AddLast(T node)
-            {
-                var newNode = new LinkedListNode<T>(node);
-
-
-                if (First == null)
-                {
-                    First = newNode;
-                    Last = First;
-                }
-                else
-                {
-                    LinkedListNode<T> previous = Last;
-                    Last.Next = newNode;
-                    Last = newNode;
-                    Last.Prev = previous;
-
-                }
-                return newNode;
+//namespace ConsoleApp2
+//{
+//    class Creategenericclasses
+//    {
+//        public class LinkedListNode<T>
+//        {
+//            public LinkedListNode(T value)
+//            {
+//                this.Value = value;
+//            }
+//            public T Value { get; internal set; }
+//            public LinkedListNode<T> Next { get; internal set; }
+//            public LinkedListNode<T> Prev { get; internal set; }
+//        }
+//        public class LinkedList<T> : IEnumerable<T>
+//        {
+//            public LinkedListNode<T> First { get; private set; }
+//            public LinkedListNode<T> Last { get; private set; }
+//            public LinkedListNode<T> AddLast(T node)
+//            {
+//                var newNode = new LinkedListNode<T>(node);
 
 
-            }
-            public IEnumerator<T> GetEnumerator()
-            {
-                LinkedListNode<T> curent = First;
-                while (curent != null)
-                {
-                    yield return curent.Value;
-                    curent = curent.Next;
-                }
-            }
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return GetEnumerator();
-            }
+//                if (First == null)
+//                {
+//                    First = newNode;
+//                    Last = First;
+//                }
+//                else
+//                {
+//                    LinkedListNode<T> previous = Last;
+//                    Last.Next = newNode;
+//                    Last = newNode;
+//                    Last.Prev = previous;
+
+//                }
+//                return newNode;
 
 
-        }
-        static void Main()
-        {
-            var List2 = new LinkedList<int>();
-            List2.AddLast(1);
-            List2.AddLast(3);
-            List2.AddLast(5);
-            foreach (int i in List2 )
-            {
-                Console.WriteLine(i);
-            }
-        }
-    }
-}
+//            }
+//            public IEnumerator<T> GetEnumerator()
+//            {
+//                LinkedListNode<T> curent = First;
+//                while (curent != null)
+//                {
+//                    yield return curent.Value;
+//                    curent = curent.Next;
+//                }
+//            }
+//            IEnumerator IEnumerable.GetEnumerator()
+//            {
+//                return GetEnumerator();
+//            }
+
+
+//        }
+//        static void Main()
+//        {
+//            var List2 = new LinkedList<int>();
+//            List2.AddLast(1);
+//            List2.AddLast(3);
+//            List2.AddLast(5);
+//            foreach (int i in List2 )
+//            {
+//                Console.WriteLine(i);
+//            }
+//        }
+//    }
+//}
