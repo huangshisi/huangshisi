d6b984a2c77844c80e0d7ac10bca77a30b0e4d88
1625941327 <1625941327@qq.com>
2019-09-30 16:17:54 +0800

    Multidimensional array

--- ConsoleApp2/Array access.cs
+++ ConsoleApp2/Array access.cs
@@ -1,29 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace ConsoleApp2
-{
-    class Class11
-    {
-        static void Main()
-        {
-        int[] Array = new int[4] { 4, 7, 11, 2 };
-        int[] myArray = new int[] { 4, 7, 11, 2 };
-        int v1 = myArray[0];
-        int v2 = myArray[1];
-        myArray[3]=44;
-            for (int i=0;i<myArray .Length;i++)
-            {
-                Console.WriteLine(myArray[i]);
-            }
-            foreach (var val in myArray )
-            {
-                Console.WriteLine(val);
-            }
-            Console.ReadLine();
+//namespace ConsoleApp2
+//{
+//    class Class11
+//    {
+//        static void Main()
+//        {
+//        int[] Array = new int[4] { 4, 7, 11, 2 };
+//        int[] myArray = new int[] { 4, 7, 11, 2 };
+//        int v1 = myArray[0];
+//        int v2 = myArray[1];
+//        myArray[3]=44;
+//            for (int i=0;i<myArray .Length;i++)
+//            {
+//                Console.WriteLine(myArray[i]);
+//            }
+//            foreach (var val in myArray )
+//            {
+//                Console.WriteLine(val);
+//            }
+//            Console.ReadLine();
 
 
-        }
-    }
-}
+//        }
+//    }
+//}
