be58c73ec449eaa18fc1240ef5637f7c82d10c5d
1625941327 <1625941327@qq.com>
2019-09-30 16:38:14 +0800

    diedaicheju

--- ConsoleApp2/Multidimensional array.cs
+++ ConsoleApp2/Multidimensional array.cs
@@ -1,37 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace ConsoleApp2
-{
-    class Class10
-    {
-        public class Person
-        {
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public override string ToString()
-            {
-                return string.Format("{0} {1}", FirstName, LastName);
+//namespace ConsoleApp2
+//{
+//    class Class10
+//    {
+//        public class Person
+//        {
+//            public string FirstName { get; set; }
+//            public string LastName { get; set; }
+//            public override string ToString()
+//            {
+//                return string.Format("{0} {1}", FirstName, LastName);
 
-            }
+//            }
 
-        }
-        static void Main()
-        {
-            Person[] myPersons = new Person[2];
-            myPersons[0] = new Person { FirstName = "Ayrton", LastName = "Senns" };
-            myPersons[1] = new Person { FirstName = "Michael", LastName = "Schumacher" };
+//        }
+//        static void Main()
+//        {
+//            Person[] myPersons = new Person[2];
+//            myPersons[0] = new Person { FirstName = "Ayrton", LastName = "Senns" };
+//            myPersons[1] = new Person { FirstName = "Michael", LastName = "Schumacher" };
            
-            int[,,] threedim = { { { 1, 2 }, { 3, 4 }
-                },
-                { { 5, 6 }, { 7, 8 }
-                }, { { 9, 10 }, { 11, 12 }
-                }
-            };
-            Console.WriteLine(threedim[0,1,1]);
-            Console.ReadLine();
+//            int[,,] threedim = { { { 1, 2 }, { 3, 4 }
+//                },
+//                { { 5, 6 }, { 7, 8 }
+//                }, { { 9, 10 }, { 11, 12 }
+//                }
+//            };
+//            Console.WriteLine(threedim[0,1,1]);
+//            Console.ReadLine();
 
-        }
-    }
-}
+//        }
+//    }
+//}
---
+++ ConsoleApp2/diedaicheju.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Class12
+    {
+        static void Main()
+        {
+            int[][] jangged = new int[3][];
+            jangged[0] = new int[2] { 1, 2 };
+            jangged[1] = new int[6] { 3,4, 5, 6, 7, 8 };
+            jangged[2] = new int[4] { 9, 10, 11, 12 };
+            for (int row=0;row<jangged .Length;row ++)
+            {
+                for(int element=0;element <jangged [row].Length;element ++)
+                {
+                    Console.WriteLine("row:{0},element{1},value:{2}", row, element, jangged[row][element]);
+                    
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
