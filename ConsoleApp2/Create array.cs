130428e1b61929ffc0f7625d7c2482e976e5e66a
1625941327 <1625941327@qq.com>
2019-09-30 17:05:17 +0800

    Create array

---
+++ ConsoleApp2/Create array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Class10
+    {
+        static void Main()
+        {
+            Array intArray1 = Array.CreateInstance(typeof(int), 5);
+            for (int i = 0; i < 5; i++)
+            {
+                intArray1.SetValue(33, i);
+            }
+            for (int i=0;i<5;i++)
+            {
+                Console.WriteLine(intArray1.GetValue(i));
+            }
+            Console.ReadLine();
+        }
+    }
+}
