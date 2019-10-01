68d666063035a69960f54a9d077a009646ed9cc1
1625941327 <1625941327@qq.com>
2019-10-01 23:47:48 +0800

    shuzuxiebian

---
+++ ConsoleApp2/shuzudexiebian.cs
@@ -0,0 +1,55 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace ConsoleApp2
+//{
+//    class Class11
+//    {
+//        internal class Person
+//        {
+//            public string FirstName { get; internal set; }
+//            public string LastName { get; internal set; }
+//        }
+//        static Person []GetPersons()
+//        {
+//            return new Person[]
+//            {
+//                new Person {FirstName ="Damon",LastName ="Hill"},
+//                new Person {FirstName ="Niki",LastName ="Lauda"},
+//                new Person {FirstName ="Ayrton",LastName ="Sonna"},
+//                new Person {FirstName ="Graham",LastName ="Hill"}
+//            };
+//        }
+//        static void Main(Person []persons)
+
+//        {
+            
+//            Array.Sort(persons);
+//            foreach (Person p in persons )
+//            {
+//                Console.WriteLine(p);
+//            }
+            
+//        }
+//        static void Main()
+//        {
+//            SortName();
+//            Person[] persons = GetPersons();
+//            SortPersons(persons);
+//            Console.WriteLine();
+          
+//            Console.ReadLine();
+//        }
+
+//        private static void SortName()
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        private static void SortPersons(Person[] persons)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
--- ConsoleApp2/sort1.cs
+++ ConsoleApp2/sort1.cs
@@ -1,45 +1,45 @@
-﻿using System;
+﻿//using System;
 
-namespace ConsoleApp2
-{
-    public class Person : IComparable<Person>
-    {
-        public string FirstName { get; set; }
+//namespace ConsoleApp2
+//{
+//    public class Person : IComparable<Person>
+//    {
+//        public string FirstName { get; set; }
 
-        public string LastName { get; set; }
+//        public string LastName { get; set; }
 
-        public override string ToString()
-        {
-            return String.Format("{0} {1}",
-                  FirstName, LastName);
-        }
+//        public override string ToString()
+//        {
+//            return String.Format("{0} {1}",
+//                  FirstName, LastName);
+//        }
 
-        public int CompareTo(Person other)
-        {
-            if (other == null) throw new ArgumentNullException("other");
+//        public int CompareTo(Person other)
+//        {
+//            if (other == null) throw new ArgumentNullException("other");
 
-            int result = this.LastName.CompareTo(other.LastName);
-            if (result == 0)
-            {
-                result = this.FirstName.CompareTo(other.FirstName);
-            }
+//            int result = this.LastName.CompareTo(other.LastName);
+//            if (result == 0)
+//            {
+//                result = this.FirstName.CompareTo(other.FirstName);
+//            }
 
-            return result;
-        }
-        static void Main()
-        {
-            Person[] persons = {new Person {FirstName="Damon",LastName="Hili"},
-  new Person {FirstName ="Niki",LastName="Lauda"},
-   new Person {FirstName ="Ayrton",LastName="Senna"},
-   new Person {FirstName ="Grahan",LastName ="Hili"} };
-            Array.Sort(persons);
-                foreach (var p in persons)
-            {
-                Console.WriteLine(p);
-            }
-            Console.ReadLine();
-        }
+//            return result;
+//        }
+//        static void Main()
+//        {
+//            Person[] persons = {new Person {FirstName="Damon",LastName="Hili"},
+//  new Person {FirstName ="Niki",LastName="Lauda"},
+//   new Person {FirstName ="Ayrton",LastName="Senna"},
+//   new Person {FirstName ="Grahan",LastName ="Hili"} };
+//            Array.Sort(persons);
+//                foreach (var p in persons)
+//            {
+//                Console.WriteLine(p);
+//            }
+//            Console.ReadLine();
+//        }
         
 
-    }
-}
+//    }
+//}
