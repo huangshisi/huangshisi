9cefcb77a008d8d7e5e7705f20c3e7808db7883f
1625941327 <1625941327@qq.com>
2019-10-02 19:59:01 +0800

    boxing1

--- ConsoleApp2/Type conversion.cs
+++ ConsoleApp2/Type conversion.cs
@@ -1,101 +1,101 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace ConsoleApp2
-{
-    class Class9
-    {
-        struct ItemDetails
-        {
-            public string Description;
-            public int ApproxPrice;
-        }
-        static void Main()
-        {
-            //byte b = 225;
-            //unchecked
-            //{
-            //    b++;
-            //}
-            //Console.WriteLine(b.ToString());
-            //Console.ReadLine();
-            //int i = 10;
-            //if (i is object)
-            //{
-            //    Console.WriteLine("i is an object");
-            //}
-            //Console.ReadLine();
-            //object o1 = "some String";
-            //object o2 = 5;
-            //string s1 = o1 as string;
-            //string s2 = o2 as string;
-            //Console.WriteLine(s1);
-            //Console.WriteLine(s2);//不能转换
-            //Console.ReadLine();
-            //int? a =null;
-            //int? b = a + 4;
-            //int? c = a * 5;
-            //Console.WriteLine(a);
-            //Console.WriteLine(b);
-            //Console.WriteLine(c);
-            //Console.ReadLine();
-            //int? a = null;
-            //int? b = 5;
-            //if (a >= b)
-            //{
-            //    Console.WriteLine("a>=b");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("a<=b");
-            //}
-            //Console.ReadLine();
-            //int? a = null;
-            //int b;int b1;
-            //b = a ?? 10;
-            //a = 3;
-            //b1 = a ?? 10;
-            //Console.WriteLine(b);
-            //Console.WriteLine(b1);
-            //Console.ReadLine();
-            //byte value1 = 10;
-            //byte value2 = 23;
-            //long  total;
-            //total = value1 + value2;
-            //Console.WriteLine(total);
-            //Console.ReadKey();
-            //long val = 3000000000;
-            //int i = (int)val;
-            //Console.WriteLine(i);
-            //Console.ReadLine();
-            //double price = 25.60;
-            //int approximatePrice = (int)(price+0.5);
-            //Console.WriteLine(approximatePrice);
-            //Console.ReadLine();
-            //ushort c = 43;
-            //char symbol = (char)c;
-            //Console.WriteLine(symbol);
-            //Console.ReadLine();
-            // double[] Prices = { 25.80, 26.60, 27.40, 30.00 };
+//namespace ConsoleApp2
+//{
+//    class Class9
+//    {
+//        struct ItemDetails
+//        {
+//            public string Description;
+//            public int ApproxPrice;
+//        }
+//        static void Main()
+//        {
+//            //byte b = 225;
+//            //unchecked
+//            //{
+//            //    b++;
+//            //}
+//            //Console.WriteLine(b.ToString());
+//            //Console.ReadLine();
+//            //int i = 10;
+//            //if (i is object)
+//            //{
+//            //    Console.WriteLine("i is an object");
+//            //}
+//            //Console.ReadLine();
+//            //object o1 = "some String";
+//            //object o2 = 5;
+//            //string s1 = o1 as string;
+//            //string s2 = o2 as string;
+//            //Console.WriteLine(s1);
+//            //Console.WriteLine(s2);//不能转换
+//            //Console.ReadLine();
+//            //int? a =null;
+//            //int? b = a + 4;
+//            //int? c = a * 5;
+//            //Console.WriteLine(a);
+//            //Console.WriteLine(b);
+//            //Console.WriteLine(c);
+//            //Console.ReadLine();
+//            //int? a = null;
+//            //int? b = 5;
+//            //if (a >= b)
+//            //{
+//            //    Console.WriteLine("a>=b");
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine("a<=b");
+//            //}
+//            //Console.ReadLine();
+//            //int? a = null;
+//            //int b;int b1;
+//            //b = a ?? 10;
+//            //a = 3;
+//            //b1 = a ?? 10;
+//            //Console.WriteLine(b);
+//            //Console.WriteLine(b1);
+//            //Console.ReadLine();
+//            //byte value1 = 10;
+//            //byte value2 = 23;
+//            //long  total;
+//            //total = value1 + value2;
+//            //Console.WriteLine(total);
+//            //Console.ReadKey();
+//            //long val = 3000000000;
+//            //int i = (int)val;
+//            //Console.WriteLine(i);
+//            //Console.ReadLine();
+//            //double price = 25.60;
+//            //int approximatePrice = (int)(price+0.5);
+//            //Console.WriteLine(approximatePrice);
+//            //Console.ReadLine();
+//            //ushort c = 43;
+//            //char symbol = (char)c;
+//            //Console.WriteLine(symbol);
+//            //Console.ReadLine();
+//            // double[] Prices = { 25.80, 26.60, 27.40, 30.00 };
 
-            //     ItemDetails id;
-            //     id.Description = "Hello there";
-            //     id.ApproxPrice = (int)(Prices[0] + 0.5);
-            //foreach (int a in Prices)
-            // {
-            //     Console.WriteLine(a);
-            // }
-            // Console.ReadLine();
-            //int i = 10;
-            //string s = i.ToString();          
-            //Console.WriteLine(s);
-            //Console.ReadLine();
-            //string s = "100";
-            //int i = int.Parse(s);
-            //Console.WriteLine(i+50);
-            //Console.ReadLine();
+//            //     ItemDetails id;
+//            //     id.Description = "Hello there";
+//            //     id.ApproxPrice = (int)(Prices[0] + 0.5);
+//            //foreach (int a in Prices)
+//            // {
+//            //     Console.WriteLine(a);
+//            // }
+//            // Console.ReadLine();
+//            //int i = 10;
+//            //string s = i.ToString();          
+//            //Console.WriteLine(s);
+//            //Console.ReadLine();
+//            //string s = "100";
+//            //int i = int.Parse(s);
+//            //Console.WriteLine(i+50);
+//            //Console.ReadLine();
 
-        }
-    }
-}
+//        }
+//    }
+//}
---
+++ ConsoleApp2/boxing1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Class11
+    {
+        static void Main()
+        {
+            int myIntNumber = 20;
+            object myObject = myIntNumber;
+            int mysecondNumber = (int)myIntNumber;
+            Console.WriteLine(mysecondNumber);
+            Console.ReadLine();
+        }
+    }
+}
