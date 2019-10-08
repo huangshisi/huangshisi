0ca047a90fcbc149a4daa1af2c94ff5a871d9240
1625941327 <1625941327@qq.com>
2019-10-09 00:30:08 +0800

    简单的 Entrust

--- ConsoleApp2/Entrust.cs
+++ ConsoleApp2/Entrust.cs
@@ -1,64 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace ConsoleApp2
-{
-    class Class11
-    {
-        struct Currency
-        {
-            public uint Dollars;
-            public ushort Cents;
-            public Currency(uint dollars, ushort cents)
-            {
-                this.Dollars = dollars;
-                this.Cents = cents;
-            }
-            public override string ToString()
-            {
-                return String.Format("${0}.{1,2:00}", Dollars, Cents);
-            }
-            public static string GetCurrencyUnit()
-            {
-                return "Dollar";
-            }
-            public static explicit operator Currency(float value)
-            {
-                checked
-                {
-                    uint dollars = (uint)value;
-                    ushort cents = (ushort)((value - dollars) * 100);
-                    return new Currency(dollars, cents);
-                }
-            }
-            public static implicit operator float(Currency value)
-            {
-                return value.Dollars + (value.Cents / 100.0f);
-            }
-            public static implicit operator Currency(uint value)
-            {
-                return new Currency(value, 0);
-            }
-            public static implicit operator uint(Currency value)
-            {
-                return value.Dollars;
-            }
-        }
-        private delegate string GetAstring();
-        static void Main()
-        {
-            int x = 40;
-            GetAstring firstStringMethod = x.ToString;
-            Console.WriteLine("String is {0}",firstStringMethod());
-            Currency balance = new Currency(34, 50);
-            firstStringMethod = balance.ToString;
+//namespace ConsoleApp2
+//{
+//    class Class11
+//    {
+//        struct Currency
+//        {
+//            public uint Dollars;
+//            public ushort Cents;
+//            public Currency(uint dollars, ushort cents)
+//            {
+//                this.Dollars = dollars;
+//                this.Cents = cents;
+//            }
+//            public override string ToString()
+//            {
+//                return String.Format("${0}.{1,2:00}", Dollars, Cents);
+//            }
+//            public static string GetCurrencyUnit()
+//            {
+//                return "Dollar";
+//            }
+//            public static explicit operator Currency(float value)
+//            {
+//                checked
+//                {
+//                    uint dollars = (uint)value;
+//                    ushort cents = (ushort)((value - dollars) * 100);
+//                    return new Currency(dollars, cents);
+//                }
+//            }
+//            public static implicit operator float(Currency value)
+//            {
+//                return value.Dollars + (value.Cents / 100.0f);
+//            }
+//            public static implicit operator Currency(uint value)
+//            {
+//                return new Currency(value, 0);
+//            }
+//            public static implicit operator uint(Currency value)
+//            {
+//                return value.Dollars;
+//            }
+//        }
+//        private delegate string GetAstring();
+//        static void Main()
+//        {
+//            int x = 40;
+//            GetAstring firstStringMethod = x.ToString;
+//            Console.WriteLine("String is {0}",firstStringMethod());
+//            Currency balance = new Currency(34, 50);
+//            firstStringMethod = balance.ToString;
            
-            Console.WriteLine("String is{0}",firstStringMethod() );
-            firstStringMethod = new GetAstring(Currency.GetCurrencyUnit);
-            Console.WriteLine("String is{0}", firstStringMethod());
-            Console.ReadLine();
-        }
+//            Console.WriteLine("String is{0}",firstStringMethod() );
+//            firstStringMethod = new GetAstring(Currency.GetCurrencyUnit);
+//            Console.WriteLine("String is{0}", firstStringMethod());
+//            Console.ReadLine();
+//        }
 
-    }
-}
+//    }
+//}
---
+++ ConsoleApp2/Entrust1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Mathoperations
+    {
+        public static double MultiplyByTwo(double value)
+        {
+            return value * 2;
+        }
+        public static double Square(double value)
+        {
+            return value * value;
+        }
+        delegate double Doubleop(double x);
+        
+        static void Main()
+        {
+            Doubleop[] operations =
+            {
+                Mathoperations.MultiplyByTwo,
+                Mathoperations.Square
+            };
+            for (int i=0;i<operations.Length;i++)
+            {
+                Console.WriteLine("Using operations{0}:",i);
+                ProcessAndDisplayNumber(operations[i], 2.0);
+                ProcessAndDisplayNumber(operations[i], 7.94);
+                ProcessAndDisplayNumber(operations[i], 1.414);
+                Console.ReadLine();
+            }
+            
+        }
+
+        static void ProcessAndDisplayNumber(Doubleop action, double value)
+        {
+            double result = action(value);
+            Console.WriteLine("Value is {0},result of operation is {1}", value, result);
+            Console.ReadLine();
+        }
+        
+    }
+}
